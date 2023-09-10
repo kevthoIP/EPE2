664b89a7fe33bf64d42986b06bcdfbcd99b03ed6
BastianUlrich-Dev <bastianjorquera8@gmail.com>
2023-09-09 22:45:44 -0300

    Agrega logica al registro de usuarios y ordena archivos en carpetas

---
+++ js/registro_usuario.js
@@ -0,0 +1,49 @@
+const registroForm = document.querySelector('#registroForm');
+const correoInput = document.querySelector('#correo');
+const nombreInput = document.querySelector('#nombre');
+const direccionInput = document.querySelector('#direccion');
+const empresaInput = document.querySelector('#empresa');
+const edadInput = document.querySelector('#edad');
+const sexoInput = document.querySelector('#sexo');
+const contrasenaInput = document.querySelector('#contrasena');
+const confirmContrasenaInput = document.querySelector('#confirmContrasena');
+
+// SUBMIT
+registroForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const correo = correoInput.value;
+    const nombre = nombreInput.value;
+    const direccion = direccionInput.value;
+    const empresa = empresaInput.value;
+    const edad = edadInput.value;
+    const sexo = sexoInput.value;
+    const contrasena = contrasenaInput.value;
+    const confirmContrasena = confirmContrasenaInput.value;
+  
+    if (contrasena !== confirmContrasena) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+  
+    const usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios') || '[]');
+    const usuarioExistente = usuariosRegistrados.find(usuario => usuario.correo === correo);
+    if (usuarioExistente) {
+      alert('El usuario ya está registrado');
+      return;
+    }
+    const usuario = {
+      correo: correo,
+      nombre: nombre,
+      direccion: direccion,
+      empresa: empresa,
+      edad: edad,
+      sexo: sexo,
+      contrasena: contrasena
+    };
+    usuariosRegistrados.push(usuario);
+    localStorage.setItem('usuarios', JSON.stringify(usuariosRegistrados));
+    alert('Usuario registrado correctamente');
+  
+    registroForm.reset();
+
+  });
